a92ed6213c899adf43cd705b30fde41728e0b172
Den <weblifeon@gmail.com>
2023-02-20 02:37:47 +0300

    last

--- WS3.ino
+++ WS3.ino
@@ -99,7 +99,7 @@ void loop() {
       if (fader[i] == 128) {
 
         //Рандом, от1  до 10 горим или таем
-        int down = random(0, 100);
+        int down = random(0, 1000);
         if (down > 0) {
           fader[i]--;
         }
